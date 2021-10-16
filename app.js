1f9203346f397e0c1527923f4de66c129d2c7c48
Ridho Tamma <58821061+ridhotamma@users.noreply.github.com>
2021-10-16 10:57:34 +0700

    adding all the files

---
+++ app.js
@@ -0,0 +1,49 @@
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const dotenv = require("dotenv");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const cors = require("cors");
+const app = express();
+const swaggerUi = require("swagger-ui-express"),
+  swaggerDocument = require("./swagger.json");
+
+dotenv.config();
+app.use(cors());
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+const port = 3000 || process.env.PORT;
+app.listen(port, () => console.log("listening to the port " + port));
+
+module.exports = app;
